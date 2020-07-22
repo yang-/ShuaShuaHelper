cc71dcc0f871c06afa98de82ef11e4812ecabaf3
Yang Chen <>
2020-07-22 18:23:01 -0400

    Add missing page matcher

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { originAndPathMatches: 'leetcode(-cn)?.com/problems/.+' },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ])
+  })
+})
+
 chrome.pageAction.onClicked.addListener((tab) => {
   chrome.tabs.update(tab.id, {
     url: tab.url.includes('leetcode.com')
